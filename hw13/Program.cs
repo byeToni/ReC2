619400f1d99da31b4e50bbe2e107e1d8dfb269fa
A_SH <hciveegress@mail.ru>
2023-07-03 20:02:22 +0300

    hw13

---
+++ hw13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 99)
+    Console.Write("третьей цифры нет ");
+else
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    Console.Write($"третья цифра:  {num % 10}");
+}
