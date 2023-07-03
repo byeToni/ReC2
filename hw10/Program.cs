a78c5cfb74e9bd3ca1bd14877b3e889758ecaf98
A_SH <hciveegress@mail.ru>
2023-07-03 19:55:32 +0300

    hw10

---
+++ hw10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num < 100 || num > 999)
+{
+    Console.Write("числол не трехзначное повторите ввод: ");
+    num = Convert.ToInt32(Console.ReadLine());
+};
+
+Console.Write($"Вторая цифра:  {num%100/10}");
